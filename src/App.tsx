790625d944c3533e36bdb7bce66a7d26eed36201
Livia89 <liviapriscilla1989@gmail.com>
2022-05-10 11:56:49 +0100

    styled-components start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+
+//import { Container } from "./App.styles";
+
+const App = () => {
+
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Tasks List</C.Header>
+      </C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
